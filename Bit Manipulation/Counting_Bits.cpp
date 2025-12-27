cb9a39791511745a62ccc4c4bce2af4e99746bee
ramansh <ramanshsharma12345@gmail.com>
2025-12-27 23:02:23 +0530

    Added Solution for Counting Bits

---
+++ Bit Manipulation/Counting_Bits.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =========================
+// 1. TYPEDEFS
+// =========================
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+
+// =========================
+// 2. DEBUGGING MAGIC
+// =========================
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        os << v[i] << (i < v.size() - 1 ? ", " : "");
+    }
+    return os << "]";
+}
+
+// =========================
+// 3. SOLUTION CLASS
+// =========================
+class Solution
+{
+public:
+    vector<int> countBits(int n)
+    {
+        vector<int> result;
+        for (int i = 0; i <= n; i++)
+        {
+            int res = __builtin_popcount(i);
+            result.push_back(res);
+        }
+
+        return result;
+    }
+};
+
+// =========================
+// 4. MAIN FUNCTION
+// =========================
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution sol;
+
+    // --- TEST CASE ---
+    int n = 10;
+    vi res = sol.countBits(n);
+    cout << res;
+
+    return 0;
+}
