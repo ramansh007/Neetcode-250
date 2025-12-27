0ecf7b8121d3a9ce8a6cc154b5940a5ce724bd00
ramansh <ramanshsharma12345@gmail.com>
2025-12-27 22:33:18 +0530

    Solutio added for Sigle Number

---
+++ Bit Manipulation/Single_number.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =========================
+// 1. TYPEDEFS
+// =========================
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+
+// =========================
+// 2. DEBUGGING MAGIC
+// =========================
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        os << v[i] << (i < v.size() - 1 ? ", " : "");
+    }
+    return os << "]";
+}
+
+// =========================
+// 3. SOLUTION CLASS
+// =========================
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            res ^= nums[i];
+        }
+
+        return res;
+    }
+};
+
+// =========================
+// 4. MAIN FUNCTION
+// =========================
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution sol;
+
+    // --- TEST CASE ---
+
+    vi nums = {3, 2, 3};
+
+    int res = sol.singleNumber(nums);
+    cout << res;
+
+    return 0;
+}
