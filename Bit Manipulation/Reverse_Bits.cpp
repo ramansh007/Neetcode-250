bb9e23ff333fc5f60443d74ea6f9aefdcca06406
ramansh <ramanshsharma12345@gmail.com>
2025-12-28 16:15:10 +0530

    Added Solution for Reverse Bits

---
+++ Bit Manipulation/Reverse_Bits.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =========================
+// 1. TYPEDEFS
+// =========================
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+
+// =========================
+// 2. DEBUGGING MAGIC
+// =========================
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        os << v[i] << (i < v.size() - 1 ? ", " : "");
+    }
+    return os << "]";
+}
+
+// =========================
+// 3. SOLUTION CLASS
+// =========================
+class Solution
+{
+public:
+    uint32_t reverseBits(uint32_t n)
+    {
+
+        uint32_t res = 0;
+
+        for (int i = 0; i < 32; i++)
+        {
+            res = res << 1;
+            res = res | (n & 1);
+            n = n >> 1;
+        }
+
+        return res;
+    }
+};
+
+// =========================
+// 4. MAIN FUNCTION
+// =========================
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution sol;
+
+    // --- TEST CASE ---
+    uint32_t n = 7;
+    uint32_t res = sol.reverseBits(n);
+    cout << bitset<32>(res);
+
+    return 0;
+}
