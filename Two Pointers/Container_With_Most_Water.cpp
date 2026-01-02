b560a1074dae4004f2670e0b7e9d787c965283ae
ramansh <ramanshsharma12345@gmail.com>
2026-01-02 12:58:32 +0530

    Added Solution for Container with most water

---
+++ Two Pointers/Container_With_Most_Water.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =========================
+// 1. TYPEDEFS
+// =========================
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+
+// =========================
+// 2. DEBUGGING MAGIC
+// =========================
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        os << v[i] << (i < v.size() - 1 ? ", " : "");
+    }
+    return os << "]";
+}
+
+// =========================
+// 3. SOLUTION CLASS
+// =========================
+class Solution
+{
+public:
+    int maxArea(vector<int> &heights)
+    {
+        int r = heights.size() - 1;
+        int l = 0;
+        int max = 0;
+
+        while (l < r)
+        {
+            int area = min(heights[l], heights[r]) * (r - l);
+
+            if (area > max)
+            {
+                max = area;
+            }
+
+            if (heights[l] < heights[r])
+                l++;
+            else
+                r--;
+        }
+
+        return max;
+    }
+};
+
+// =========================
+// 4. MAIN FUNCTION
+// =========================
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution sol;
+
+    // --- TEST CASE ---
+    vi heights = {2, 2, 2};
+    int res = sol.maxArea(heights);
+    // cout << res;
+
+    return 0;
+}
