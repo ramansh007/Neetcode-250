8a3e4ded4093e2c722e303b8025f1b1c1d71207e
ramansh <ramanshsharma12345@gmail.com>
2026-01-02 12:02:59 +0530

    Added Solution for Rotate Array

---
+++ Two Pointers/Rotate_Array.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =========================
+// 1. TYPEDEFS
+// =========================
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+
+// =========================
+// 2. DEBUGGING MAGIC
+// =========================
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        os << v[i] << (i < v.size() - 1 ? ", " : "");
+    }
+    return os << "]";
+}
+
+// =========================
+// 3. SOLUTION CLASS
+// =========================
+class Solution
+{
+public:
+    void rotate(vector<int> &nums, int k)
+    {
+
+        int n = nums.size();
+
+        reverse(nums.end() - (k % n), nums.end());
+        reverse(nums.begin(), nums.end() - (k % n));
+        reverse(nums.begin(), nums.end());
+    }
+};
+// =========================
+// 4. MAIN FUNCTION
+// =========================
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution sol;
+
+    // --- TEST CASE ---
+    vi nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+
+    sol.rotate(nums, k);
+    cout << nums;
+    return 0;
+}
