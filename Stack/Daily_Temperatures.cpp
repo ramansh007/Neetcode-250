d9c158dcac98305bcb2f39fd6d015a6bb13fe273
ramansh <ramanshsharma12345@gmail.com>
2026-01-14 17:21:28 +0530

    Added Solution for Daily Temperature

---
+++ Stack/Daily_Temperatures.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =========================
+// 1. TYPEDEFS
+// =========================
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using pii = pair<int, int>;
+
+// =========================
+// 2. DEBUGGING MAGIC
+// =========================
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        os << v[i] << (i < v.size() - 1 ? ", " : "");
+    }
+    return os << "]";
+}
+
+// =========================
+// 3. SOLUTION CLASS
+// =========================
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+
+        int n = temperatures.size();
+        vector<int> res;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            int temp = temperatures[i];
+            int j = i + 1;
+            int count = 0;
+            while (j < n)
+            {
+                // cout << "temp " << temp << " temp-2 " << temperatures[j] << endl;
+
+                if (temp < temperatures[j])
+                {
+                    count++;
+                    break;
+                }
+                else
+                {
+                    count++;
+                    j++;
+                }
+            }
+
+            if (j < n)
+            {
+                res.push_back(count);
+            }
+            else
+            {
+                res.push_back(0);
+            }
+        }
+
+        return res;
+    }
+};
+
+// =========================
+// 4. MAIN FUNCTION
+// =========================
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution sol;
+
+    // --- TEST CASE ---
+    vi temperatures = {30, 38, 30, 36, 35, 40, 28};
+    vi res = sol.dailyTemperatures(temperatures);
+    cout << res;
+
+    return 0;
+}
